cbe9a1d0df9890fd55b8d7b47fa26f6e4211ab0e
AdrianPGM <adrianmatheuspgm@gmail.com>
2024-12-19 19:14:54 -0300

    upload_netflix_clone

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+let botaSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".infor")
+let modal =document.querySelector(".modal")
+let audio = document.querySelector(".audio")
+
+
+botaSom.addEventListener("click", ligaSom )
+
+function ligaSom() {
+    video.muted = false
+};
+
+function ligaSom(){
+    video.muted = !video.muted
+}
+
+botao.addEventListener("click", mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal(){
+    modal.style.display = "block"
+}
+
+function esconderModal(){
+    modal.style.display ="none"
+}
+
+window.addEventListener("load", audioTocar)
+
+function audioTocar(){
+    audio.play()
+}
